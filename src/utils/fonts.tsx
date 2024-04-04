b98d3678f90164865dcf49c4314bce0eb2ad43b4
camille <106037608+rirum@users.noreply.github.com>
2024-04-04 17:34:38 -0300

    first commit

---
+++ src/utils/fonts.tsx
@@ -0,0 +1,7 @@
+import { Passion_One, Erica_One } from 'next/font/google';
+
+export const ericaOne = Erica_One({ subsets: ['latin'], weight: '400' });
+export const passionOne = Passion_One({
+  subsets: ['latin'],
+  weight: '400',
+});
