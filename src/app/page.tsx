bfdd49699eac05651c17d75685e81e08df01809e
camille <106037608+rirum@users.noreply.github.com>
2024-04-05 17:38:08 -0300

    feat: login page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 'use client';
 import styled from 'styled-components';
-import { ericaOne } from '@/utils/fonts';
+
+import { passionOne, fredoka } from '@/utils/fonts';
 
 export default function Home() {
   return (
-    <Background className={ericaOne.className}>
-      <h1>embroidery list</h1>
+    <Background className={fredoka.className}>
+      <WrapperText>
+        <StyledText>
+          <h1>embroidery</h1>
+
+          <WrapperLoginAndAbout>
+            <h2>list</h2>
+            <LoginAndAbout className={passionOne.className}>
+              <p>login</p>
+              <p>about us</p>
+            </LoginAndAbout>
+          </WrapperLoginAndAbout>
+        </StyledText>
+      </WrapperText>
     </Background>
   );
 }
@@ -15,9 +28,45 @@ const Background = styled.div`
   height: 100vh;
   background-color: #ffb2b2;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WrapperText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledText = styled.div`
   h1 {
-    margin-top: 0;
-    font-size: 36px;
+    margin: 0;
+    font-size: 150px;
+    color: #ca0000;
+  }
+  h2 {
+    line-height: 0.6;
+    font-size: 400px;
+    color: #ca0000;
+  }
+`;
+
+const WrapperLoginAndAbout = styled.div`
+  display: flex;
+`;
+
+const LoginAndAbout = styled.div`
+  display: flex;
+  height: 280px;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  margin-left: 30px;
+
+  p {
+    font-size: 50px;
     color: #ca0000;
   }
 `;
--- src/utils/fonts.tsx
+++ src/utils/fonts.tsx
@@ -1,7 +1,9 @@
-import { Passion_One, Erica_One } from 'next/font/google';
+import { Passion_One, Erica_One, Fredoka } from 'next/font/google';
 
 export const ericaOne = Erica_One({ subsets: ['latin'], weight: '400' });
 export const passionOne = Passion_One({
   subsets: ['latin'],
   weight: '400',
 });
+
+export const fredoka = Fredoka({ subsets: ['latin'], weight: '700' });
